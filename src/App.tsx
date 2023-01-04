c4158d61d789f7764fad72ec1788f010f0999671
Leila Daraei <107926155+LeilaDaraei@users.noreply.github.com>
2023-01-04 17:21:59 +0100

    added form

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,16 @@ function App() {
   return (
     <div className="container">
       <h1>React with typescript</h1>
+      <form>
+        <label htmlFor="userName">Name:</label>
+        <input id="userName" type="text" name="name" value="" />
+        <label htmlFor="userAge">Age:</label>
+        <input id="userAge" type="number" name="age" value="" />
 
+        <label htmlFor="userJob">Job:</label>
+        <input id="userJob" type="text" name="job" value="" />
+        <button type="submit">Add user</button>
+      </form>
     </div>
   );
 }
