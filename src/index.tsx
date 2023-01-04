13efdb27ed7b21cef65237b682518fc1a5fe9ed9
Leila Daraei <107926155+LeilaDaraei@users.noreply.github.com>
2023-01-04 21:37:35 +0100

    added footer

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <App />
+    <footer>
+      This project is coded by <a href="https:daraei.me">Leila Daraei</a> and is{" "}
+      <a href="https://github.com/LeilaDaraei/user-list-typescript">
+        {" "}
+        open-sourced on GitHub
+      </a>
+      .
+    </footer>
   </React.StrictMode>
 );
 
